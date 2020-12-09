8de73cb77601afc42af23ba89958571d88d7c544
tuke307 <tonymeissner70@gmail.com>
2020-12-09 15:25:54 +0100

    add GetLocalisedRes function

---
+++ LapTimer.Forms.UI/Functions/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace LapTimer.Forms.UI.Functions
+{
+    public static class Functions
+    {
+        public static string GetLocalisedRes(Type resType, string resourceNameKey)
+        {
+            string translate = string.Empty;
+
+            try
+            {
+                ResourceManager rm = new ResourceManager(resType);
+                translate = rm.GetString(resourceNameKey, CultureInfo.CurrentCulture);
+            }
+            catch
+            {
+                translate = string.Empty;
+            }
+            return translate;
+        }
+    }
+}
