7e9895611b9070f25665616612213a083aa482d2
tuke307 <tonymeissner70@gmail.com>
2020-12-21 20:03:11 +0100

    add models

---
+++ LapTimer.Forms.UI/Models/HumanEffortComputer.cs
@@ -0,0 +1,23 @@
+﻿using LapTimer.Forms.UI.Themes;
+using SkiaSharpnado.Maps.Domain;
+using System.Collections.Generic;
+
+namespace LapTimer.Forms.UI.Models
+{
+    public static class HumanEffortComputer
+    {
+        public static EffortComputer BySpeed { get; }
+
+        static HumanEffortComputer()
+        {
+            BySpeed = new EffortComputer(
+                new List<EffortSpan>
+                    {
+                        new EffortSpan(0f, ResourcesHelper.GetResourceColor("ColorEffortSpeedMin"), /*Functions.Resources.GetLocalisedRes(typeof(Resx.resources),nameof(Resx.resources.PaceVeryLight)*/"VERY LIGHT"),
+                        new EffortSpan(0.7f, ResourcesHelper.GetResourceColor("ColorEffortAnaerobic"), /*Functions.Resources.GetLocalisedRes(typeof(Resx.resources),nameof(Resx.resources.PaceAnaerobic)*/"ANAEROBIC"),
+                        new EffortSpan(0.95f, ResourcesHelper.GetResourceColor("ColorEffortMax"), /*Functions.Resources.GetLocalisedRes(typeof(Resx.resources),nameof(Resx.resources.PaceMax)*/"MAX"),
+                    },
+                40);
+        }
+    }
+}
---
+++ LapTimer.Forms.UI/Models/ValueBounds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LapTimer.Forms.UI.Models
+{
+    public struct ValueBounds
+    {
+        public double Max { get; }
+
+        public double Min { get; }
+
+        public ValueBounds(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
