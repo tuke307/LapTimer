bbfda952abd213ae40c80e836cde72ff40146bde
tuke307 <tonymeissner70@gmail.com>
2021-01-12 22:10:20 +0100

    add test project

---
+++ LapTimer.Test/CoreViewModelsTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LapTimer.Test
+{
+    public class CoreViewModelsTest : MvxTest
+    {
+        [Test]
+        public void ViewModelTest()
+        {
+        }
+
+        protected override void AdditionalSetup()
+        {
+            base.AdditionalSetup();
+        }
+    }
+}
